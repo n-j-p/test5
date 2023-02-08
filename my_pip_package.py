187f562303f282ef7fcbf29b84daefc8e3bdd68a
NJP <n.j.p.coder@gmail.com>
2023-02-08 21:14:02 +1100

    initial

---
+++ my_pip_package.py
@@ -0,0 +1,5 @@
+def hello_world():
+    print('Hi  asdf')
+from sklearn.base import BaseEstimator
+class ExTransformer(BaseEstimator):
+    pass    
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my_pip_package',
+    version=0.1,
+
+    url='https://github.com/n-j-p/test5',
+    author='n-j-p',
+
+    py_modules=['my_pip_package'],
+)
